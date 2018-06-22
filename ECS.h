3ac2faa904334adfa016e24544681e973220f0a7
Alistair Miles <ajrmiles@gmail.com>
2018-06-22 12:53:38 +0100

    Corrected project settings for a DLL and gtest

--- ECS.h
+++ ECS.h
@@ -1,13 +1,7 @@
 #pragma once
 
 #ifdef ECS_EXPORTS  
-	#define ECS_API __declspec(dllexport)   
+#define ECS_API __declspec(dllexport)   
 #else  
-	#define ECS_API __declspec(dllimport)   
+#define ECS_API __declspec(dllimport)   
 #endif  
-
-namespace ECS
-{
-	class ECS_API Entity {
-	};
-}
---
+++ Entity.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ECS.h"
+#include <bitset>
+
+namespace ECS {
+	class ECS_API Entity {
+	public:
+	private:
+	};
+}
+
+
