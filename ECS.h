3b1c076d42973c3402a62e4e2369a028cf4013ba
Alistair Miles <ajrmiles@gmail.com>
2018-06-26 10:41:21 +0100

    Developing the library as a DLL was not going to work due to the nature of DLL boundaries. Converted to a static library and cleaned the project.

--- ECS.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef ECS_EXPORTS  
-#define ECS_API __declspec(dllexport)   
-#else  
-#define ECS_API __declspec(dllimport)   
-#endif  
-
-#define MAX_COMPONENTS 32
--- test/Component.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "../Component.h"
-
-TEST(Component, ComponentTypesHaveUniqueIDs) {
-	struct ComponentA : ECS::Component {};
-	struct ComponentB : ECS::Component {};
-	ASSERT_TRUE(ECS::GetTypeIdentifier<ComponentA>() != ECS::GetTypeIdentifier<ComponentB>());
-}
